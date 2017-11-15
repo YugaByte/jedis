9a87e678dc5cd63c83eecaf692da8dad98f2938d
Hector Cuellar <hector@yugabyte.com>
2017-11-15 11:08:46 -0800

    Update license and other meta-data in Jedis for release to Maven Central
    Summary: Changes to YugaByte Jedis client necessary to release it to Maven Central
    
    Test Plan: Pending
    
    Reviewers: mikhail, pritam.damania, robert
    
    Reviewed By: robert
    
    Differential Revision: https://phabricator.dev.yugabyte.com/D3413
    
    Conflicts:
    	pom.xml

--- src/main/java/redis/clients/jedis/Commands.java
+++ src/main/java/redis/clients/jedis/Commands.java
@@ -1,3 +1,19 @@
+/**
+ *   The following only applies to changes made to this file as part of YugaByte development.
+ *
+ *      Portions Copyright (c) YugaByte, Inc.
+ *
+ *   Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file
+ *   except in compliance with the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *   Unless required by applicable law or agreed to in writing, software distributed under the
+ *   License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND,
+ *   either express or implied.  See the License for the specific language governing permissions
+ *   and limitations under the License.
+ */
+
 package redis.clients.jedis;
 
 import redis.clients.jedis.BinaryClient.LIST_POSITION;
@@ -159,7 +175,7 @@ public interface Commands {
                     final String expire_cmd, final long expire_time);
 
   public void tsget(final String key, final long timestamp);
-  
+
   public void tsrem(final String key, final long... timestamps);
 
   public void tsrangeByTime(final String key, final long min, final long max);
