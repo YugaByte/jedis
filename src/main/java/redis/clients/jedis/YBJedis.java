f12e18481038f51a5f4cb7e262dd098ceee0d768
Mihnea Iancu <mihnea@yugabyte.com>
2018-04-17 13:58:40 -0700

    Quickfix for SET with nxxx option in YBJedis
    Summary:
    JedisCluster forwards this case to SETNX which we don't support yet.
    So we temporarily override the method in YBJedis to ensure we call SET not SETNX.
    
    Test Plan: YB Jedis tests
    
    Reviewers: amitanand, hector
    
    Reviewed By: hector
    
    Subscribers: yql
    
    Differential Revision: https://phabricator.dev.yugabyte.com/D4633

--- src/main/java/redis/clients/jedis/YBJedis.java
+++ src/main/java/redis/clients/jedis/YBJedis.java
@@ -439,4 +439,15 @@ public class YBJedis extends JedisCluster implements AdvancedJedisCommands, Adva
       }
     }.run();
   }
+
+  // TODO We should remove this when YB supports SETNX.
+  @Override
+  public String set(final String key, final String value, final String nxxx) {
+    return new JedisClusterCommand<String>(connectionHandler, maxAttempts) {
+      @Override
+      public String execute(Jedis connection) {
+        return connection.set(key, value, nxxx);
+      }
+    }.run(key);
+  }
 }
